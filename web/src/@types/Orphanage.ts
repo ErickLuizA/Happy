f6749a8dd794f2c7ff2c359a9ba510c0dcc5e26e
erickLuizA <erickluiz69@gmail.com>
2020-10-14 15:49:36 -0300

    day 3

---
+++ web/src/@types/Orphanage.ts
@@ -0,0 +1,17 @@
+
+export default interface OrphanagesProps {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+  about: string
+  instructions: string
+  opening_hours: string
+  open_on_weekends: boolean
+  images: [
+    {
+      id: number
+      url: string
+    }
+  ]
+}
